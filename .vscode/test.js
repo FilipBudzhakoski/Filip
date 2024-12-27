e33d0959bd479091f7708b0bceea5558a18b481d
Filip Budzhakoski <fbudzakoski@hotmail.com>
2024-12-27 17:10:36 +0100

    Filip

---
+++ .vscode/test.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+$("#firstWrapper h1").text("NEW H1 WITH JQUERY");
+$("#firstWrapper h1").after("<p>I ADDED NEW PARAGRAPH WITH JQUERY</p>");
+$("button").css("background-color", "red");
+  });
+
+  paragraph.last().hide();
+  paragraph.all().css("background-color","green");
+
+  btnText.text("Don`t click me");
+  
+  
